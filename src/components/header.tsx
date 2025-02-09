287a3256f2251a40748ad6a4cf33b77f72b77a01
cubar4 <cubar4@proton.me>
2025-02-10 06:59:46 +0900

    Working on it...

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,26 @@
+"use client"
+
 import {NavigationMenu} from "@/components/ui/navigation-menu";
 import Avatar from "@/components/profile/Avatar";
+import Image from "next/image";
+import logo from "../../public/logo.png"
 
 export default function Header() {
     return (
         <NavigationMenu className="h-[69px] max-w-none w-full p-[1rem]">
             <div className="flex flex-row grid-rows-3 w-full justify-between items-center">
-                <div>
-                    <p>test</p>
+                <div className="h-[49px] flex grid-row items-center">
+                    <a className="h-[49px] flex grid-row items-center">
+                        <Image className="h-full max-h-full w-auto" src={logo} alt="Logo" />
+                        <div>
+                            Studio
+                        </div>
+                    </a>
                 </div>
-                <div>
+                <div className="h-[49px] flex grid-row items-center">
 
                 </div>
-                <div>
+                <div className="h-[49px] flex grid-row items-center">
                     <Avatar />
                 </div>
             </div>
