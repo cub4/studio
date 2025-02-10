080f123d7841b116a2238831f31bd9743d7e8bf6
cub4 <150250473+cub4@users.noreply.github.com>
2025-02-10 20:24:47 +0900

    Working on it...

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,17 +2,18 @@
 
 import {NavigationMenu} from "@/components/ui/navigation-menu";
 import Avatar from "@/components/profile/Avatar";
+import Link from "next/link";
 
 export default function Header() {
     return (
         <NavigationMenu className="h-[69px] max-w-none w-full p-[1rem]">
             <div className="flex flex-row grid-rows-3 w-full justify-between items-center">
                 <div className="h-[49px] flex grid-row items-center">
-                    <a href="/" className="h-[49px] flex grid-row items-center">
+                    <Link href="/" className="h-[49px] flex grid-row items-center">
                         <div className="font-[Teko] text-2xl">
                             W/S Studio
                         </div>
-                    </a>
+                    </Link>
                 </div>
                 <div className="h-[49px] flex grid-row items-center">
 
